3e37fa5bb9b3027de0de70f0c021d1e17fa1b737
Ibrahim Gazi <coder.ibrahimgazi@gmail.com>
2025-07-04 00:28:19 +0600

    Add vercel.json

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
-const PORT = process.env.PORT || 5000;
 
 const app = express();
+
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5000", "https://pos-app-mern-prod.vercel.app"],
@@ -17,14 +17,20 @@ app.use(cors({
     credentials: true
 }));
 
-//  Routes
-app.get('/', (req, res) => {
+// Routes
+app.get("/", (req, res) => {
     res.json("Backend Running");
 });
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-    console.log(
-        `Node Server Running In ${process.env.NODE_ENV} ModeOn Port ${process.env.PORT}`
-    );
-});
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(
+            `Node Server Running In ${process.env.NODE_ENV} Mode on Port ${PORT}`
+        );
+    });
+}
+
+// ✅ Export app for Vercel serverless function
+export default app;
